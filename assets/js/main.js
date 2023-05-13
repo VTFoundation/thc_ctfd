3b987dd400ba3f7ca4bc6c289b987cf3cabf2577
evan-vicich <126218725+evan-vicich@users.noreply.github.com>
2023-05-13 12:20:24 -0700

    Updated index.html
    -Updated scroll up button (added css and js)
    -Updated YouTube link to display in Glightbox
    -commented out the blog section
    -Made single service boxes hyperlinks

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,16 @@
     "use strict";
 
 }) ();
+
+
+//Scroll to top 
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
